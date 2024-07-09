1d7b9a1b6a89e9b01081500448ffea3d2e27eab2
MrTophat72 <128369641+MrTophat72@users.noreply.github.com>
2024-07-09 16:54:35 +0200

    Moving Camera
    Camera works

--- Assets/Scripts/GameScripts/CameraMovements.cs
+++ Assets/Scripts/GameScripts/CameraMovements.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed;
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position = transform.position + new Vector3(move.x, 0, move.y) * Time.deltaTime*speed;
         
     }
 }
